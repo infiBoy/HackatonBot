fff4545b952c59975061ba630847b52971a0798e
HAIM-PC\HAIM <hiambrum@gmail.com>
2017-02-15 01:13:20 +0200

    wikipedia script

---
+++ libs/wikiScript.py
@@ -0,0 +1,14 @@
+import re
+import wikipedia
+
+
+
+def wikicomment(subject):
+    data = wikipedia.page("israel")
+    try:
+        newContent = data.content.split("== Background ==")
+        newContent = newContent[1][:130]
+        print newContent
+    except:
+        print "No wikipedia Background"
+
