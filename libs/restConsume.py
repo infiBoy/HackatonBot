fb40872931a96784d5d05987e36dbff1b7f05838
HAIM-PC\HAIM <hiambrum@gmail.com>
2017-02-14 12:45:49 +0200

    Merge branch 'master' of https://github.com/infiBoy/HackatonBot
    # Conflicts:
    #	skills/crawling.py

--- libs/restConsume.py
+++ libs/restConsume.py
@@ -10,28 +10,29 @@ fileName ="Pro"
 
 import credential
 try:
-    tso = TwitterSearchOrder() # create a TwitterSearchOrder object
-    tso.set_keywords(['israel', 'palestine']) # let's define all words we would like to have a look for
-    tso.set_language('en') # we want to see German tweets only
-    tso.set_include_entities(False) # and don't give us all those entity information
+    while True:
+        tso = TwitterSearchOrder() # create a TwitterSearchOrder object
+        tso.set_keywords(['israel', 'crime']) # let's define all words we would like to have a look for
+        tso.set_language('en') # we want to see German tweets only
+        tso.set_include_entities(False) # and don't give us all those entity information
 
-    # it's about time to create a TwitterSearch object with our secret tokens
-    ts = TwitterSearch(
+        # it's about time to create a TwitterSearch object with our secret tokens
+        ts = TwitterSearch(
 
-        consumer_key = credential.CONSUMER_KEY,
-        consumer_secret = credential.CONSUMER_SECRET,
-        access_token = credential.ACCESS_KEY,
-        access_token_secret = credential.ACCESS_SECRET
-     )
+            consumer_key = credential.CONSUMER_KEY,
+            consumer_secret = credential.CONSUMER_SECRET,
+            access_token = credential.ACCESS_KEY,
+            access_token_secret = credential.ACCESS_SECRET
+         )
 
-     # this is where the fun actually starts :)
-    for tweet in ts.search_tweets_iterable(tso):
-        print tweet['text']
-        with open( fileName+ '.json', 'a') as f:
+         # this is where the fun actually starts :)
+        for tweet in ts.search_tweets_iterable(tso):
+            print tweet['text']
+            with open( fileName+ '.json', 'a') as f:
 
-            f.write(tweet['text'].encode('ascii', 'ignore').decode('ascii') + "|||Positive \n") #Changeme for negative
+                f.write(tweet['text'].encode('ascii', 'ignore').decode('ascii') + "|||Positive \n") #Changeme for negative
 
-        #print( '@%s tweeted: %s' % ( tweet['user']['screen_name'], tweet['text'] ) )
+            #print( '@%s tweeted: %s' % ( tweet['user']['screen_name'], tweet['text'] ) )
 
 except TwitterSearchException as e: # take care of all those ugly errors if there are some
     print(e)
