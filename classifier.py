396172db4d3f0152c62bc0d3f2f5b24b607728ea
Tomer781 <Tomer7811@gmail.com>
2017-02-14 11:21:18 +0200

    Merge branches 'Tomer' and 'master' of https://github.com/infiBoy/HackatonBot into Tomer
    # Conflicts:
    #	BasicHello.py

--- classifier.py
+++ classifier.py
@@ -4,42 +4,41 @@ from textblob.classifiers import NaiveBayesClassifier
 import cPickle as pickle
 import os.path
 
-cl = None
-if(os.path.isfile('trainedBrain.pkl')) :
-    with open('trainedBrain.pkl', 'rb') as input:
-        cl = pickle.load(input)
+class classifier :
+    cl = None
 
-random.seed(1)
+    def learnAndSave(self) :
+        if(os.path.isfile('trainedBrain.pkl') and self.cl == None) :
+            with open('trainedBrain.pkl', 'rb') as input:
+                self.cl = pickle.load(input)
 
-# Grab some movie review data
-#reviews = [(list(movie_reviews.words(fileid)), category)
-#              for category in movie_reviews.categories()
-#             for fileid in movie_reviews.fileids(category)]
-#random.shuffle(reviews)
-#train, test = reviews[0:1000], reviews[1001:1400]
+        random.seed(1)
 
-twitts = [];
+        # Grab some movie review data
+        #reviews = [(list(movie_reviews.words(fileid)), category)
+        #              for category in movie_reviews.categories()
+        #             for fileid in movie_reviews.fileids(category)]
+        #random.shuffle(reviews)
+        #train, test = reviews[0:1000], reviews[1001:1400]
 
-with open("twitts.txt") as f:
-    input = f.readlines()
-    for twitt in input :
-         twitts.append((twitt.split("|||||")[0], twitt.split("|||||")[1]))
+        twitts = [];
 
-random.shuffle(twitts)
-train, test = twitts[0:twitts.__len__() * 0.7], twitts[twitts.__len__() * 0.7 + 1:twitts.__len__()]
+        with open("twitts.txt") as f:
+            input = f.readlines()
+            for twitt in input :
+                twitts.append((twitt.split("|||||")[0], twitt.split("|||||")[1]))
+
+        random.shuffle(twitts)
+        train, test = twitts[0:twitts.__len__() * 0.7], twitts[twitts.__len__() * 0.7 + 1:twitts.__len__()]
 
 
-if(cl == None) :
-    cl = NaiveBayesClassifier(train)
-else :
-    cl.update(train)
+        if(self.cl == None) :
+            self.cl = NaiveBayesClassifier(train)
+        else :
+            self.cl.update(train)
 
-# Compute accuracy
-accuracy = cl.accuracy(test)
-print("Accuracy: {0}".format(accuracy))
+        with open('trainedBrain.pkl', 'wb') as output:
+            pickle.dump(self.cl, output, pickle.HIGHEST_PROTOCOL)
 
-# Show 5 most informative features
-cl.show_informative_features(5)
-
-with open('trainedBrain.pkl', 'wb') as output:
-    pickle.dump(cl, output, pickle.HIGHEST_PROTOCOL)
+    def classify(self, twitt):
+        return self.cl.classify(twitt)
